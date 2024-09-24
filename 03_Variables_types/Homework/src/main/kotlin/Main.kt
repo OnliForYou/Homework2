aa8fc90519a02b01c3b7cd47479a36381f3b0455
Timur <wilkawilka01@gmail.com>
2024-09-24 13:03:50 +0300

    add homework

---
+++ 03_Variables_types/Homework/src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+    var firstName = "Timur"
+    var lastName = "Galev"
+    var height = 176.toDouble()
+    var weight = 76f
+    var heightChild = 150
+    var weightChild = 40
+
+    val isChild: Boolean
+    isChild = height >= heightChild && weight >= weightChild
+
+    val info = """
+Имя: $firstName
+Фамилия: $lastName
+Рост: $height
+Вес: $weight
+   """.trimIndent()
+    println(info)
+
+    println("Он не ребенок: $isChild")
+
+}
